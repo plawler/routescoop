1f38ea8d656ae209d3ee4b9a6d1727f69e25ef1d
Paul Lawler <paul.lawler@gmail.com>
2018-09-23 14:25:15 -0400

    prevent the ride sync unless settings are created

--- routescoop-web/app/controllers/Rides.scala
+++ routescoop-web/app/controllers/Rides.scala
@@ -1,24 +1,28 @@
 package controllers
 
 import javax.inject.Inject
-import models.{Profile, RideSyncResultStarted}
+import models.{Profile, RideSyncResultStarted, SettingsResultSuccess}
 import modules.NonBlockingContext
 import play.api.Logger
 import play.api.cache.CacheApi
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.libs.ws.WSClient
 import play.api.mvc.{Controller, Request}
-import services.RideService
+import services.{RideService, SettingsService}
 
 import scala.concurrent.{ExecutionContext, Future}
 
 class Rides @Inject()(
   authenticated: AuthenticatedAction,
   rideService: RideService,
+  settingsService: SettingsService,
   ws: WSClient,
   cache: CacheApi,
   val messagesApi: MessagesApi)
   (implicit @NonBlockingContext ec: ExecutionContext) extends Controller with I18nSupport {
+
+  val syncRidesUrl = routes.Rides.sync()
+  val createSettingsUrl = routes.Settings.create()
 
   def sync = authenticated.async { implicit request =>
     getProfile(request) match {
@@ -33,8 +37,15 @@ class Rides @Inject()(
     }
   }
 
-  def index = authenticated  { implicit request =>
-    Ok(views.html.rides.index(routes.Rides.sync()))
+  def index = authenticated.async { implicit request =>
+    getProfile(request) map { profile =>
+      hasSettings(profile.id) map { hasSettings =>
+        Ok(views.html.rides.index(hasSettings, if (hasSettings) syncRidesUrl else createSettingsUrl))
+      }
+    } getOrElse {
+      Logger.error("A profile wasn't found in cache for the user...logging out")
+      Future.successful(Redirect(routes.Auth.logout()))
+    }
   }
 
   private def getProfile(request: Request[Any]): Option[Profile] = {
@@ -43,6 +54,13 @@ class Rides @Inject()(
       profile <- cache.get[Profile](sessionId + "profile")
     } yield {
       profile
+    }
+  }
+
+  private def hasSettings(userId: String): Future[Boolean] = {
+    settingsService.list(userId) map {
+      case SettingsResultSuccess(settings) => settings.nonEmpty
+      case _ => false
     }
   }
 

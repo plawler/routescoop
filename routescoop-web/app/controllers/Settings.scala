c0594bd1fea7d037c3305bea9a6d466ee07e7e0d
plawler <paul.lawler@gmail.com>
2019-03-19 06:56:52 -0400

    clean up the profile reference in the action builder (#34)

--- routescoop-web/app/controllers/Settings.scala
+++ routescoop-web/app/controllers/Settings.scala
@@ -35,49 +35,28 @@ class Settings @Inject()(
   }
 
   def save = authenticated.async { implicit request =>
-    getProfile(request) match {
-      case Some(profile) =>
-        SettingsForm.form.bindFromRequest.fold(
-          formWithErrors =>
-            Future.successful(BadRequest(views.html.settings.create(formWithErrors, postUrl))),
-          data => {
-            settingsService.create(toNewSettings(profile, data)) map {
-              case srs: SettingsResultSuccess => Redirect(listUrl).flashing("success" -> "Settings saved")
-              case _ => Redirect(listUrl).flashing("error" -> "Attempt to save settings failed")
-            } recover {
-              case NonFatal(e) =>
-                Logger.error(s"failed to save settings $e")
-                Redirect(listUrl).flashing("error" -> "Attempt to save settings failed to complete")
-            }
-          }
-        )
-      case None =>
-        Logger.error("A profile wasn't found in cache for the user...logging out")
-        Future.successful(Redirect(routes.Auth.logout()))
-    }
+    SettingsForm.form.bindFromRequest.fold(
+      formWithErrors =>
+        Future.successful(BadRequest(views.html.settings.create(formWithErrors, postUrl))),
+      data => {
+        settingsService.create(toNewSettings(request.profile, data)) map {
+          case srs: SettingsResultSuccess => Redirect(listUrl).flashing("success" -> "Settings saved")
+          case _ => Redirect(listUrl).flashing("error" -> "Attempt to save settings failed")
+        } recover {
+          case NonFatal(e) =>
+            Logger.error(s"failed to save settings $e")
+            Redirect(listUrl).flashing("error" -> "Attempt to save settings failed to complete")
+        }
+      }
+    )
   }
 
   def list = authenticated.async { implicit request =>
-    getProfile(request) match {
-      case Some(profile) =>
-        settingsService.list(profile.id) map {
-          case SettingsResultSuccess(settings) => Ok(views.html.settings.list(settings))
-          case SettingsResultError(message) =>
-            Logger.error(s"Failed to retrieve settings: $message")
-            Ok(views.html.settings.list(Seq())).flashing("error" -> "Failed to retrieve settings")
-        }
-      case None =>
-        Logger.error("A profile wasn't found in cache for the user...logging out")
-        Future.successful(Redirect(routes.Auth.logout()))
-    }
-  }
-
-  private def getProfile(request: Request[Any]): Option[Profile] = {
-    for {
-      sessionId <- request.session.get("idToken")
-      profile <- cache.get[Profile](sessionId + "profile")
-    } yield {
-      profile
+    settingsService.list(request.profile.id) map {
+      case SettingsResultSuccess(settings) => Ok(views.html.settings.list(settings))
+      case SettingsResultError(message) =>
+        Logger.error(s"Failed to retrieve settings: $message")
+        Ok(views.html.settings.list(Seq())).flashing("error" -> "Failed to retrieve settings")
     }
   }
 

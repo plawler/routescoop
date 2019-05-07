4acf29750185a179e94d75b4eb0ae15e19992727
plawler <paul.lawler@gmail.com>
2019-05-07 08:35:57 -0400

    test for missing intervals

--- routescoop-api/test/repositories/PowerEffortStoreSpec.scala
+++ routescoop-api/test/repositories/PowerEffortStoreSpec.scala
@@ -36,7 +36,9 @@ class PowerEffortStoreSpec extends WordSpec with Matchers with PowerEffortStoreF
 
     "get maximum power efforts" in {
       effortStore.getMaximalEfforts(testUser.id, 365, Seq(1, 5, 30, 60, 360)) shouldNot be(empty)
+      effortStore.getMaximalEfforts(testUser.id, 365, Seq(1, 5, 60, 360)) should be(empty)
     }
+
   }
 
 }

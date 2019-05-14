4919da89784563497a24ed4bfbfcc59df55764bd
plawler <paul.lawler@gmail.com>
2019-05-14 06:37:03 -0400

    test three observation requirement

--- routescoop-api/test/metrics/LinearRegressionSpec.scala
+++ routescoop-api/test/metrics/LinearRegressionSpec.scala
@@ -7,13 +7,19 @@ import org.scalatest.{FlatSpec, Matchers}
 class LinearRegressionSpec extends FlatSpec with Matchers with RegressionFixture {
 
   it should "find the slope and y intercept of a line" in {
-    truncate(model.slope, 1) shouldEqual 265.5d
-    roundUp(model.intercept, 0) shouldEqual 20964
+    truncate(model.slope, 1) shouldEqual 265.5d // equal to critical power
+    roundUp(model.intercept, 0) shouldEqual 20964 // w' or (awc) is the y intercept
     roundUp(model.rSquared, 4) shouldEqual 0.9994d
   }
 
   it should "predict the y value for x" in {
     roundUp(model.predict(3600), 0) shouldEqual 976899
+  }
+
+  it should "require at least three observations" in {
+    the [IllegalArgumentException] thrownBy {
+      new LinearRegression(observations.dropRight(2))
+    } should have message "requirement failed: At least three observations are required to run linear regression"
   }
 
 }

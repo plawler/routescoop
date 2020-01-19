046183894dbf2131c459cafd86acaf1b08134fde
plawler <paul.lawler@gmail.com>
2020-01-19 12:06:11 -0500

    added additional durations to extend the cp curve in both directions

--- routescoop-api/test/services/FitnessServiceSpec.scala
+++ routescoop-api/test/services/FitnessServiceSpec.scala
@@ -38,13 +38,13 @@ class FitnessServiceSpec extends WordSpec with Matchers with MockitoSugar {
       val intervals = Seq(180, 360, 720)
       when(mockPowerEffortStore.getMaximalEfforts(userId, lookback, intervals)).thenReturn(samples)
       val cp = service.getCriticalPower(userId, lookback, intervals)
-      cp.predictions shouldBe Seq(CriticalPowerPrediction(60,631), CriticalPowerPrediction(120,445), CriticalPowerPrediction(180,383), CriticalPowerPrediction(240,352), CriticalPowerPrediction(300,334), CriticalPowerPrediction(480,306), CriticalPowerPrediction(600,296), CriticalPowerPrediction(900,284), CriticalPowerPrediction(1200,278), CriticalPowerPrediction(2400,269), CriticalPowerPrediction(3600,265))
+      cp.predictions shouldBe Seq(CriticalPowerPrediction(15,1747), CriticalPowerPrediction(30,1003), CriticalPowerPrediction(60,631), CriticalPowerPrediction(120,445), CriticalPowerPrediction(180,383), CriticalPowerPrediction(240,352), CriticalPowerPrediction(300,334), CriticalPowerPrediction(480,306), CriticalPowerPrediction(600,296), CriticalPowerPrediction(900,284), CriticalPowerPrediction(1200,278), CriticalPowerPrediction(2400,269), CriticalPowerPrediction(3600,265), CriticalPowerPrediction(7200,262), CriticalPowerPrediction(9800,261))
       cp.cp shouldBe 258.7
       cp.wPrime shouldBe 22320.0
     }
 
     "simulate critical power" in new Fixture {
-      val resultsJson = """{"simulationType":"CriticalPower","result":{"cp":354.7,"wPrime":11520,"predictions":[{"duration":60,"watts":547},{"duration":120,"watts":451},{"duration":180,"watts":419},{"duration":240,"watts":403},{"duration":300,"watts":394},{"duration":480,"watts":379},{"duration":600,"watts":374},{"duration":900,"watts":368},{"duration":1200,"watts":365},{"duration":2400,"watts":360},{"duration":3600,"watts":358}]}}""".stripMargin
+      val resultsJson = """{"simulationType":"CriticalPower","result":{"cp":354.7,"wPrime":11520,"predictions":[{"duration":15,"watts":1123},{"duration":30,"watts":739}, {"duration":60,"watts":547},{"duration":120,"watts":451},{"duration":180,"watts":419},{"duration":240,"watts":403},{"duration":300,"watts":394},{"duration":480,"watts":379},{"duration":600,"watts":374},{"duration":900,"watts":368},{"duration":1200,"watts":365},{"duration":2400,"watts":360},{"duration":3600,"watts":358},{"duration":7200,"watts":357},{"duration":9800,"watts":356}]}}""".stripMargin
       val cp = service.simulateCriticalPower(simulation)
       Json.toJson(cp) shouldEqual Json.parse(resultsJson)
     }

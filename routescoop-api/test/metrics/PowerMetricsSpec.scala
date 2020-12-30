d0a7bc4eac3cb2ba93b9b40b14fadb157b3c4371
plawler <paul.lawler@gmail.com>
2020-12-30 08:53:22 -0500

    VO2max estimator function (#66)
    Created a function to estimate vo2max based on ACSM vo2max calculation. Appropriate for cycling.

--- routescoop-api/test/metrics/PowerMetricsSpec.scala
+++ routescoop-api/test/metrics/PowerMetricsSpec.scala
@@ -62,9 +62,19 @@ class PowerMetricsSpec extends FlatSpec with Matchers with Fixture {
     variabilityIndex(0, 1)
   }
 
+  it should "estimate vo2max" in {
+    estimateVO2max(maxAerobicPower, weightInKg, VerticalConstant, RestingConstant) shouldEqual 61.0d
+  }
+
 }
 
 trait Fixture extends StressScoreFixture {
   val data = Seq(100, 150, 200, 100, 150, 150, 150, 200, 100, 200)
   val interval = 3
+  val VerticalConstant = 10.8d
+  val VerticalConstantMILFIT = 12.35d
+  val RestingConstant = 7.0d
+  val RestingConstantMILFIT = 3.5d
+  val maxAerobicPower = 350
+  val weightInKg = 70.0d
 }

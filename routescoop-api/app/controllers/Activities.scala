0809927249be1d76dc662a1150c5af6705bd85b3
plawler <paul.lawler@gmail.com>
2020-12-31 00:24:30 -0500

    Support activity stats updates in api
    In order to make changes to the activity stats manually, a PUT endpoint is necessary for activity stats. This will enable a client application to send a payload to alter the stats generated for an activity in case one of the metrics cannot be generated. For example, if no power data, we need to manually assign the training load for the activity.

--- routescoop-api/app/controllers/Activities.scala
+++ routescoop-api/app/controllers/Activities.scala
@@ -1,14 +1,17 @@
 package controllers
 
 import javax.inject.{Inject, Singleton}
+import models.ActivityStats
 import modules.{AppConfig, NonBlockingContext}
 import services.{ActivityService, PowerAnalysisService}
 
 import com.typesafe.scalalogging.LazyLogging
-import play.api.libs.json.Json
+import play.api.libs.json.{JsError, Json}
 import play.api.mvc.{BaseController, ControllerComponents}
 
+import java.sql.{SQLException, SQLIntegrityConstraintViolationException}
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.Success
 
 @Singleton
 class Activities @Inject()(
@@ -22,6 +25,20 @@ class Activities @Inject()(
     activityService.fetchActivities(userId, page, config.pageSize) map { summaries =>
       Ok(Json.toJson(summaries))
     }
+  }
+
+  def updateStats = Action.async(parse.json) { implicit request =>
+    request.body.validate[ActivityStats].fold(
+      errors => Future.successful(BadRequest(JsError.toJson(errors))),
+      stats => {
+        activityService.getActivity(stats.activityId) map {
+          case Some(_) =>
+            powerAnalysisService.updateActivityStats(stats)
+            NoContent
+          case None => BadRequest(s"Invalid activity stats ${Json.toJson(stats)}")
+        }
+      }
+    )
   }
 
   def getPowerDistribution(activityId: String) = Action.async { implicit request =>
